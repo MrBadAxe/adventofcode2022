86446794bb79300ace41abe8b3f3ead5a7ad0b8e
MrBadAxe <mrbadaxe@gmail.com>
2022-12-15 19:07:35 -0500

    supplemental Point class for when you just need an ordered 2-tuple

---
+++ Point.java
@@ -0,0 +1,29 @@
+public class Point{
+  private final int x;
+  private final int y;
+
+  public Point(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+
+  public Point(){
+    this(0,0);
+  }
+
+  public int getX(){
+    return this.x;
+  }
+
+  public int getY(){
+    return this.y;
+  }
+
+  @Override
+  public boolean equals(Object o){
+    if(o == this){ return true; }
+    if(!(o instanceof Point)){ return false; }
+    Point other = (Point)o;
+    return (this.getX() == other.getX() && this.getY() == other.getY());
+  }
+}
