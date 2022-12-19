1389204233798e30628a17a53f2e317715b0d6e7
MrBadAxe <mrbadaxe@gmail.com>
2022-12-19 00:16:35 -0500

    Day08: refactor to use main() readInput, move grid generation to separate method

--- Day08.java
+++ Day08.java
@@ -1,42 +1,27 @@
 //package org.mrbadaxe.AdventOfCode2022;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.List;
 
 public class Day08{
 
-  private static ArrayList<String> lines;
-  private static Day08TreeGrid grid;
-
-  public static void readInput(String filepath){
-    try{
-      BufferedReader console = new BufferedReader(new FileReader(filepath));
-      lines = new ArrayList<String>();
-      String nextLine = null;
-      while((nextLine=console.readLine())!=null){
-        lines.add(nextLine);
-      }
-    }catch(java.io.IOException e){
-      System.err.println("IOException: " + e.getMessage());
-    }
-
-    int width = lines.get(0).length();
-    int height = lines.size();
-    grid = new Day08TreeGrid(width,height);
+  public static Day08TreeGrid generateGrid(List<String> input){
+    int width = input.get(0).length();
+    int height = input.size();
+    Day08TreeGrid grid = new Day08TreeGrid(width,height);
     for(int cx=0; cx<grid.getWidth(); cx++){
-      String[] cells = lines.get(cx).split("");
+      String[] cells = input.get(cx).split("");
       for(int cy=0; cy<grid.getHeight(); cy++){
         grid.set(cx,cy,Integer.parseInt(cells[cy]));
       }
     }
+    return grid;
   }
 
-  public static int getPart01(String filepath){
-    readInput(filepath);
+  public static int getPart01(List<String> input){
+    Day08TreeGrid grid = generateGrid(input);
     int countVisible = 0;
     for(int cx=0; cx<grid.getWidth(); cx++){
-      String[] cells = lines.get(cx).split("");
+      String[] cells = input.get(cx).split("");
       for(int cy=0; cy<grid.getHeight(); cy++){
         if(grid.isVisible(cx,cy)){ countVisible++; }
       }
@@ -44,15 +29,14 @@ public class Day08{
     return countVisible;
   }
 
-  public static int getPart02(String filepath){
-    readInput(filepath);
+  public static int getPart02(List<String> input){
+    Day08TreeGrid grid = generateGrid(input);
     int bestView = 0;
     for(int cx=0; cx<grid.getWidth(); cx++){
-      String[] cells = lines.get(cx).split("");
+      String[] cells = input.get(cx).split("");
       for(int cy=0; cy<grid.getHeight(); cy++){
         bestView = Math.max(bestView, grid.getScenicScore(cx,cy));
       }
-      //System.out.print("\n");
     }
     return bestView;
   }
