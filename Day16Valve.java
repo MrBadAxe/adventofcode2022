9c9f097e050c877b1c14555a049cf37dd7c1782c
MrBadAxe <mrbadaxe@gmail.com>
2022-12-19 17:28:09 -0500

    Day 16: create Valve class

---
+++ Day16Valve.java
@@ -0,0 +1,44 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day16Valve{
+  private boolean openState;
+  private String name;
+  private int flowRate;
+  private List<Day16Valve> links;
+
+  public Day16Valve(String name, int rate){
+    this.openState = false;
+    this.name = name;
+    this.flowRate = rate;
+    this.links = new ArrayList<Day16Valve>();
+  }
+
+  public String getName(){
+    return this.name;
+  }
+  public int getFlowRate(){
+    return this.flowRate;
+  }
+  public List<Day16Valve> getLinks(){
+    return this.links;
+  }
+  public boolean isOpen(){
+    return this.openState;
+  }
+  public void open(){
+    this.openState = true;
+  }
+
+  public String toString(){
+    String z = this.getName() + "(" + this.getFlowRate() + ")->";
+    for(Day16Valve link : links){
+      z += link.getName() + ",";
+    }
+    return z;
+  }
+
+  public void linkTo(Day16Valve other){
+    this.getLinks().add(other);
+  }
+}
