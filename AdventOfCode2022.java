fd6fd24eefca497337afd4b57a09b31ba6c6abbf
MrBadAxe <mrbadaxe@gmail.com>
2022-12-19 00:06:31 -0500

    Day07: refactor to use main() readInput, remove debug prints

--- AdventOfCode2022.java
+++ AdventOfCode2022.java
@@ -30,39 +30,37 @@ public class AdventOfCode2022{
 
     System.out.println("Day 05 Part 01: " + Day05.getPart01(readInput("./input_day05.txt")));
     System.out.println("Day 05 Part 02: " + Day05.getPart02(readInput("./input_day05.txt")));
-    */
+
     System.out.println("Day 06 Part 01: " + Day06.getPart01(readInput("./input_day06.txt")));
     System.out.println("Day 06 Part 02: " + Day06.getPart02(readInput("./input_day06.txt")));
-    /*
-    System.out.println("Day 07 Part 01: " + Day07.getPart01("./input_day07.txt"));
-    System.out.println("Day 07 Part 02: " + Day07.getPart02("./input_day07.txt"));
-
-    System.out.println("Day 08 Part 01: " + Day08.getPart01("./input_day08.txt"));
-    System.out.println("Day 08 Part 02: " + Day08.getPart02("./input_day08.txt"));
-
-    System.out.println("Day 09 Part 01: " + Day09.getPart01("./input_day09.txt"));
-    System.out.println("Day 09 Part 02: " + Day09.getPart02("./input_day09.txt"));
-
-    System.out.println("Day 10 Part 02: " + Day10.getPart02("./input_day10.txt"));
-    System.out.println("Day 10 Part 01: " + Day10.getPart01("./input_day10.txt"));
-
-    System.out.println("Day 11 Part 01: " + Day11.getPart01("./input_day11.txt"));
-    System.out.println("Day 11 Part 02: " + Day11.getPart02("./input_day11.txt"));
-
-    System.out.println("Day 12 Part 01: " + Day12.getPart01("./input_day12.txt"));
-    System.out.println("Day 12 Part 02: " + Day12.getPart02("./input_day12.txt"));
-
-    System.out.println("Day 13 Part 01: " + Day13.getPart01("./input_day13.txt"));
-    System.out.println("Day 13 Part 02: " + Day13.getPart02("./input_day13.txt"));
-
-    System.out.println("Day 14 Part 01: " + Day14.getPart01("./input_day14.txt"));
-    System.out.println("Day 14 Part 02: " + Day14.getPart02("./input_day14.txt"));
     */
+    System.out.println("Day 07 Part 01: " + Day07.getPart01(readInput("./input_day07.txt")));
+    System.out.println("Day 07 Part 02: " + Day07.getPart02(readInput("./input_day07.txt")));
+    /*
+    System.out.println("Day 08 Part 01: " + Day08.getPart01(readInput("./input_day08.txt")));
+    System.out.println("Day 08 Part 02: " + Day08.getPart02(readInput("./input_day08.txt")));
 
-    //System.out.println("Day 15 Part 01: " + Day15.getPart01("./input_day15_short.txt",10));
-    //System.out.println("Day 15 Part 01: " + Day15.getPart01("./input_day15.txt",2000000));
-    //System.out.println("Day 15 Part 02: " + Day15.getPart02("./input_day15_short.txt",20));
-    //System.out.println("Day 15 Part 02: " + Day15.getPart02("./input_day15.txt",4000000));
+    System.out.println("Day 09 Part 01: " + Day09.getPart01(readInput("./input_day09.txt")));
+    System.out.println("Day 09 Part 02: " + Day09.getPart02(readInput("./input_day09.txt")));
+
+    System.out.println("Day 10 Part 02: " + Day10.getPart02(readInput("./input_day10.txt")));
+    System.out.println("Day 10 Part 01: " + Day10.getPart01(readInput("./input_day10.txt")));
+
+    System.out.println("Day 11 Part 01: " + Day11.getPart01(readInput("./input_day11.txt")));
+    System.out.println("Day 11 Part 02: " + Day11.getPart02(readInput("./input_day11.txt")));
+
+    System.out.println("Day 12 Part 01: " + Day12.getPart01(readInput("./input_day12.txt")));
+    System.out.println("Day 12 Part 02: " + Day12.getPart02(readInput("./input_day12.txt")));
+
+    System.out.println("Day 13 Part 01: " + Day13.getPart01(readInput("./input_day13.txt")));
+    System.out.println("Day 13 Part 02: " + Day13.getPart02(readInput("./input_day13.txt")));
+
+    System.out.println("Day 14 Part 01: " + Day14.getPart01(readInput("./input_day14.txt")));
+    System.out.println("Day 14 Part 02: " + Day14.getPart02(readInput("./input_day14.txt")));
+
+    System.out.println("Day 15 Part 01: " + Day15.getPart01(readInput("./input_day15.txt"),2000000));
+    System.out.println("Day 15 Part 02: " + Day15.getPart02(readInput("./input_day15.txt"),4000000));
+    */
 
   }
 }
--- Day07.java
+++ Day07.java
@@ -1,49 +1,27 @@
 //package org.mrbadaxe.AdventOfCode2022;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Day07{
-  private static ArrayList<String> lines;
 
-  public static void readInput(String filepath){
-    try{
-      BufferedReader console = new BufferedReader(new FileReader(filepath));
-      lines = new ArrayList<String>();
-      String nextLine = null;
-      while((nextLine=console.readLine())!=null){
-        lines.add(nextLine);
-      }
-    }catch(java.io.IOException e){
-      System.err.println("IOException: " + e.getMessage());
-    }
-  }
-
-  public static Day07Directory buildFileTree(){
+  public static Day07Directory buildFileTree(List<String> input){
     Day07Directory root = new Day07Directory();
     Day07Directory workingDir = root;
-    for(String str : lines){
-      //System.out.println(str);
+    for(String str : input){
       String[] tokens = str.split("\s");
       if(tokens[0].equals("$")){
         if(tokens[1].equals("ls")){
-          //System.err.println("list");
         }else if(tokens[1].equals("cd")){
           if(tokens[2].equals("..")){
-            //System.err.println("up one");
             workingDir = workingDir.getParent();
           }else if(tokens[2].equals("/")){
-            //System.err.println("to root");
             workingDir = root;
           }else{
-            //System.err.println("to dir " + tokens[2]);
             workingDir = workingDir.getDir(tokens[2]);
           }
         }
       }else if(tokens[0].equals("dir")){
-        //System.err.println("new dir: " + tokens[1]);
         workingDir.mkDir(tokens[1]);
       }else{
         workingDir.addFile(tokens[1],Integer.parseInt(tokens[0]));
@@ -51,9 +29,8 @@ public class Day07{
     }
     return root;
   }
-  public static int getPart01(String filepath){
-    readInput(filepath);
-    Day07Directory root = buildFileTree();
+  public static int getPart01(List<String> input){
+    Day07Directory root = buildFileTree(input);
 
     int allTheSmallDirs = 0;
     LinkedList<Day07Directory> queue = new LinkedList<Day07Directory>();
@@ -70,9 +47,8 @@ public class Day07{
     return allTheSmallDirs;
   }
 
-  public static int getPart02(String filepath){
-    readInput(filepath);
-    Day07Directory root = buildFileTree();
+  public static int getPart02(List<String> input){
+    Day07Directory root = buildFileTree(input);
 
     int diskSize = 70_000_000;
     int freeSpaceRequired = 30_000_000;
