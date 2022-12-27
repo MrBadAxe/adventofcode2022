488db76a39b226b16ea0a2e7ff249eb823a26ea7
MrBadAxe <mrbadaxe@gmail.com>
2022-12-27 00:42:28 -0500

    Day 20 stub class

---
+++ Day20.java
@@ -0,0 +1,13 @@
+//package org.mrbadaxe.AdventOfCode2022;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day20{
+
+  public static int getPart01(List<String> input){
+    return 0;
+  }
+
+
+}
