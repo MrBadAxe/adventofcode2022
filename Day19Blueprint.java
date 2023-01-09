def37cd725bc278d87671e50f8cc7531e9272cd3
MrBadAxe <mrbadaxe@gmail.com>
2023-01-08 22:45:59 -0500

    create a new MiningOperation class and separate all the stuff unrelated to robot costs from the Blueprint class

--- Day19Blueprint.java
+++ Day19Blueprint.java
@@ -4,126 +4,49 @@ import java.util.ArrayList;
 
 public class Day19Blueprint{
 
-  private int timeElapsed;
-
   public static String[] resourceTypes = {"ore","clay","obsidian","geodes"};
 
-  private HashMap<String,Integer> resources;
-  private HashMap<String,Integer> robotCounts;
   private HashMap<String,HashMap<String,Integer>> robotCosts;
 
   public Day19Blueprint(){
-    timeElapsed = 0;
-    resources = new HashMap<String,Integer>();
-    robotCounts = new HashMap<String,Integer>();
     robotCosts = new HashMap<String,HashMap<String,Integer>>();
-
     for(String str : resourceTypes){
-      resources.put(str,0);
-      robotCounts.put(str,(str.equals("ore") ? 1 : 0));
       robotCosts.put(str,new HashMap<String,Integer>());
     }
   }
 
   public String toString(){
     String z = "";
-    z += "TIME: " + timeElapsed + " | ";
-    z += "RESOURCES: ";
-    for(String str : resourceTypes){
-      z += (str + " " + resources.get(str) + " ");
-    }
-    z += "| ROBOTS: ";
-    for(String str : resourceTypes){
-      z += (str + " " + robotCounts.get(str) + " ");
-    }
-    z += "| ACTIONS: ";
-    for(String str : this.availableBuildActions()){
-      z += str + " ";
+    for(String robot : resourceTypes){
+      z += (robot + ": ");
+      for(String cost : robotCosts.get(robot).keySet()){
+        z += (robotCosts.get(robot).get(cost) + " " + cost + " ");
+      }
+      z += "; ";
     }
     return z;
   }
 
-  public int timeElapsed(){
-    return this.timeElapsed;
+  public int getRobotCost(String robotType, String resource){
+    return (robotCosts.get(robotType).get(resource) == null ? 0 : robotCosts.get(robotType).get(resource));
   }
-  public int getResourceCount(String resourceName){
-    return resources.get(resourceName);
-  }
-  private void setResourceCount(String resourceName, int count){
-    resources.put(resourceName,count);
-  }
-  public int getRobotCount(String resourceName){
-    return robotCounts.get(resourceName);
-  }
-  private void setRobotCount(String resourceName, int count){
-    robotCounts.put(resourceName,count);
-  }
-  private void setTimeElapsed(int elapsed){
-    this.timeElapsed = elapsed;
-  }
-
   public void setRobotCost(String robotType, String resource, int q){
     if(robotCosts.get(robotType) == null){
       robotCosts.put(robotType,new HashMap<String,Integer>());
     }
     robotCosts.get(robotType).put(resource,q);
   }
-  public boolean canBuildRobot(String resourceName){
-    boolean z = true;
-    HashMap<String,Integer> cost = robotCosts.get(resourceName);
-    for(String str : cost.keySet()){
-      z &= (resources.get(str) >= cost.get(str));
+
+  public int maxRobotsNeeded(String resourceName){
+    int z = 0;
+    for(String str : resourceTypes){
+      int cost = (robotCosts.get(str).get(resourceName) == null ? 0 : robotCosts.get(str).get(resourceName));
+      z = Math.max(z,cost);
+    }
+    if(resourceName.equals("geodes")){
+      z = Integer.MAX_VALUE;
     }
     return z;
   }
 
-  public List<String> availableBuildActions(){
-    List<String> z = new ArrayList<String>();
-    z.add("gather");
-    String bestRobot = "none";
-    for(String str : resourceTypes){
-      if(canBuildRobot(str)){
-        bestRobot = str;
-      }
-    }
-    if(!bestRobot.equals("none")){ z.add(bestRobot); }
-    return z;
-  }
-
-  public Day19Blueprint copy(){
-    Day19Blueprint z = new Day19Blueprint();
-    for(String str : resourceTypes){
-      z.setResourceCount(str,this.getResourceCount(str));
-      z.setRobotCount(str,this.getRobotCount(str));
-      z.setTimeElapsed(this.timeElapsed);
-      for(String cost : this.robotCosts.get(str).keySet()){
-        z.setRobotCost(str,cost,this.robotCosts.get(str).get(cost));
-      }
-    }
-    //System.out.println(z.toString());
-    //System.out.println(z.timeElapsed() == this.timeElapsed());
-    return z;
-  }
-
-  public void tick(String action){
-    //spend to build
-    if(!action.equals("gather")){
-      for(String str : robotCosts.get(action).keySet()){
-        resources.put(str,resources.get(str)-(robotCosts.get(action).get(str) == null ? 0 : robotCosts.get(action).get(str)));
-      }
-    }
-
-    //gather
-    for(String str : resourceTypes){
-      resources.put(str,resources.get(str)+robotCounts.get(str));
-    }
-
-    //add robot
-    if(!action.equals("gather")){
-      robotCounts.put(action,robotCounts.get(action)+1);
-    }
-
-    //advance timer
-    timeElapsed++;
-  }
 }
---
+++ Day19MiningOperation.java
@@ -0,0 +1,125 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day19MiningOperation{
+
+  private int timeElapsed;
+
+  private HashMap<String,Integer> resources;
+  private HashMap<String,Integer> robots;
+  private Day19Blueprint blueprint;
+
+  public Day19MiningOperation(Day19Blueprint bp){
+    timeElapsed = 0;
+    resources = new HashMap<String,Integer>();
+    robots = new HashMap<String,Integer>();
+    blueprint = bp;
+
+    for(String str : blueprint.resourceTypes){
+      resources.put(str,0);
+      robots.put(str,0);
+    }
+    robots.put("ore",1);
+  }
+
+  public Day19MiningOperation copy(){
+    Day19MiningOperation z = new Day19MiningOperation(this.blueprint);
+    z.setTimeElapsed(this.timeElapsed);
+    for(String str : this.blueprint.resourceTypes){
+      z.setResources(str,this.resources(str));
+      z.setRobots(str,this.robots(str));
+    }
+    return z;
+  }
+
+  public String toString(){
+    String z = "";
+    z += "TIME: " + this.timeElapsed + " | ";
+    z += "RESOURCES: ";
+    for(String str : this.blueprint.resourceTypes){
+      z += (str + " " + resources.get(str) + " ");
+    }
+    z += "| ROBOTS: ";
+    for(String str : this.blueprint.resourceTypes){
+      z += (str + " " + robots.get(str) + " ");
+    }
+    z += "| ACTIONS: ";
+    for(String str : this.availableBuildActions()){
+      z += str + " ";
+    }
+    return z;
+  }
+
+  public int timeElapsed(){
+    return this.timeElapsed;
+  }
+  private void setTimeElapsed(int elapsed){
+    this.timeElapsed = elapsed;
+  }
+
+  public int resources(String resourceName){
+    return resources.get(resourceName);
+  }
+  private void setResources(String resourceName, int count){
+    resources.put(resourceName,count);
+  }
+
+  public int robots(String resourceName){
+    return robots.get(resourceName);
+  }
+  private void setRobots(String resourceName, int count){
+    robots.put(resourceName,count);
+  }
+
+
+  public boolean canBuildRobot(String resourceName){
+    boolean z = true;
+    //HashMap<String,Integer> cost = robotCosts.get(resourceName);
+    for(String str : blueprint.resourceTypes){
+      z &= (resources.get(str) >= blueprint.getRobotCost(resourceName,str));
+    }
+    return z;
+  }
+  public List<String> availableBuildActions(){
+    List<String> z = new ArrayList<String>();
+    for(int k=0;k<blueprint.resourceTypes.length;k++){
+      String str = blueprint.resourceTypes[blueprint.resourceTypes.length-(k+1)];
+      if(canBuildRobot(str)){
+        z.add(str);
+      }
+    }
+    z.add("gather");
+    return z;
+  }
+  public void tick(String action){
+    //spend to build
+    if(!action.equals("gather")){
+      for(String str : blueprint.resourceTypes){
+        resources.put(str,resources.get(str)-(blueprint.getRobotCost(action,str)));
+      }
+    }
+
+    //gather
+    for(String str : blueprint.resourceTypes){
+      resources.put(str,resources.get(str)+robots.get(str));
+    }
+
+    //add robot
+    if(!action.equals("gather")){
+      robots.put(action,robots.get(action)+1);
+    }
+
+    //advance timer
+    timeElapsed++;
+  }
+
+  public Day19MiningOperation endState(int maxTime){
+    Day19MiningOperation z = this.copy();
+    while(z.timeElapsed() < maxTime){
+      z.tick("gather");
+    }
+    return z;
+  }
+
+}
