7dc92c176f798c491ac227d18b66b3b8ca79471f
MrBadAxe <mrbadaxe@gmail.com>
2023-01-16 23:35:24 -0500

    no message

---
+++ Day24.java
@@ -0,0 +1,10 @@
+//package org.mrbadaxe.AdventOfCode2022;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Day24{
+  public static int getPart01(List<String> input){
+    return 0;
+  }
+}
