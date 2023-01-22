da3db419f12a7e854180f9c0cb88b581579bbabe
MrBadAxe <mrbadaxe@gmail.com>
2023-01-22 16:55:48 -0500

    day24: add input parse function

--- Day24.java
+++ Day24.java
@@ -4,6 +4,23 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Day24{
+  public static Day24BlizzardField parseBlizzardField(List<String> input){
+    Day24BlizzardField field = new Day24BlizzardField(input.size(),input.get(0).length());
+    for(int row=0;row<input.size();row++){
+      String line = input.get(row);
+      for(int col=0;col<line.length();col++){
+        char s = line.charAt(col);
+        switch(s){
+          case '.': field.set(row,col,0); break;
+          case '>': field.set(row,col,1 << Day24BlizzardField.FACING_RIGHT); break;
+          case 'v': field.set(row,col,1 << Day24BlizzardField.FACING_DOWN); break;
+          case '<': field.set(row,col,1 << Day24BlizzardField.FACING_LEFT); break;
+          case '^': field.set(row,col,1 << Day24BlizzardField.FACING_UP); break;
+        }
+      }
+    }
+    return field;
+  }
   public static int getPart01(List<String> input){
     return 0;
   }
